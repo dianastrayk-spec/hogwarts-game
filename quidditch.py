41754728eecc2f5ba742af8edf21e57825892b5f
dianastrayk-spec <dianastrayk@gmail.com>
2026-09-14 23:58:40 +0300

    Update quidditch.py

--- quidditch.py
+++ quidditch.py
@@ -352,7 +352,44 @@ if st.session_state.matches:
     for match in st.session_state.matches:
         for team in [match["team_a"], match["team_b"]]:
              if team not in stats:
-                stats[team] = {"Игры": 0, "Победы": 0, "Поражения": 0, "Очки": 0, "Снитчи": 0}
+                stats[team] = {
+                    "Игры": 0, 
+                    "Победы": 0, 
+                    "Поражения": 0,        # сумма всех набранных очков 
+                    "Очки": 0,        # сумма всех набранных очков 
+                    "Снитчи": 0}
+ 
+        stats[match["team_a"]]["Игры"] += 1
+        stats[match["team_b"]]["Игры"] += 1
+
+            # Добавляем реальные очки, набранные в матче
+        stats[match["team_a"]]["Очки"] += match["score_a"]
+        stats[match["team_b"]]["Очки"] += match["score_b"]
+
+            # Победы и поражения
+        if match["score_a"] > match["score_b"]:
+            stats[match["team_a"]]["Победы"] += 1
+            stats[match["team_b"]]["Поражения"] += 1
+        elif match["score_b"] > match["score_a"]:
+            stats[match["team_b"]]["Победы"] += 1
+            stats[match["team_a"]]["Поражения"] += 1
+
+            # Снитчи
+        if match["snitch"] == match["team_a"]:
+             stats[match["team_a"]]["Снитчи"] += 1
+        elif match["snitch"] == match["team_b"]:
+            stats[match["team_b"]]["Снитчи"] += 1
+
+    import pandas as pd
+    df = pd.DataFrame.from_dict(stats, orient="index")
+    df = df.sort_values(by=["Очки", "Победы", "Снитчи"], ascending=False)
+    st.dataframe(df, use_container_width=True)
+
+    # Победитель
+    leader = df.index[0]
+    st.success(f"Сейчас лидирует: **{leader}** с {df.loc[leader, 'Очки']} очками")
+else:
+    st.info("Нет данных для таблицы")
 
         stats[match["team_a"]]["Игры"] += 1
         stats[match["team_b"]]["Игры"] += 1
